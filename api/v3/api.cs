ff2cdebb5c7004d24b341926562f581c8e77d925
Kelvin Dules <kelvindules@outlook.com>
2018-03-15 20:21:49 -0300

    shrunk the example for demonstration-only purposes

--- api/v3/api.cs
+++ api/v3/api.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Web.Script.Serialization;
@@ -32,113 +31,29 @@ namespace Helpers.AddressProvider.Providers
             }
         }
 
-        public abstract class CEPAbertoRequest
+        public CEPAbertoResponse Make(String cep)
         {
-            private String BaseUrl = "http://www.cepaberto.com/api/v3/";
-            public abstract String Resource { get; }
-            public abstract String[] AvaiableParameters { get; }
+            var token = "Token token=YOUR_TOKEN";
+            var url = "http://www.cepaberto.com/api/v3/cep?cep={0}";
 
-            private String _token;
-            public String Token
-            {
-                get { return this._token; }
-                set
-                {
-                    this._token = "Token token=" + value;
-                }
-            }
+            var client = new WebClient { Encoding = Encoding.UTF8 };
+            client.Headers.Add(HttpRequestHeader.Authorization, token);
 
-            private Tuple<String, String>[] _parameters;
-            public Tuple<String, String>[] Parameters
-            {
-                get
-                {
-                    return this._parameters;
-                }
-                set
-                {
-                    var isAvaiable = false;
-                    foreach(var v in value)
-                    {
-                        isAvaiable = AvaiableParameters.Contains(v.Item1);
-                        if(!isAvaiable)
-                        {
-                            throw new Exception("Parameter not avaiable for this resource.");
-                        }
-                    }
+            var requestResult = client.DownloadString(string.Format(url, cep));
 
-                    this._parameters = value;
-                }
-            }
+            var jss = new JavaScriptSerializer();
+            var response = jss.Deserialize<CEPAbertoResponse>(requestResult);
 
-            public String DownloadString
-            {
-                get
-                {
-                    var url = BaseUrl + Resource + "?";
-                    for(int x = 0; x < Parameters.Length; x++)
-                    {
-                        url += Parameters[x].Item1 + "=" + Parameters[x].Item2;
-                        if (x < Parameters.Length - 1)
-                            url += "&";
-                    }
-                    return url;
-                }
-            }
-
-            public CEPAbertoResponse Make(CEPAbertoRequest request)
-            {
-                if (String.IsNullOrEmpty(request.Token))
-                {
-                    throw new Exception("Token can't be null or empty.");
-                }
-
-                var client = new WebClient { Encoding = Encoding.UTF8 };
-                client.Headers.Add(HttpRequestHeader.Authorization, request.Token);
-                var requestResult = client.DownloadString(request.DownloadString);
-
-                var jss = new JavaScriptSerializer();
-                var response = jss.Deserialize<CEPAbertoResponse>(requestResult);
-
-                return response;
-            }
-            public CEPAbertoResponse Make()
-            {
-                return Make(this);
-            }
-
-            public CEPAbertoRequest(String token, Tuple<String,String>[] parameters)
-            {
-                Token = token;
-                Parameters = parameters;
-            }
+            return response;
         }
 
-        public class ByCEP : CEPAbertoRequest
+        public void Test()
         {
-            // call base constructor
-            public ByCEP(String token, Tuple<String, String>[] parameters) : base(token, parameters) { }
-            public override String Resource
-            {
-                get { return "cep"; }
-            }
-            public override String[] AvaiableParameters
-            {
-                get { return new String[] { "cep" }; }
-            }
-        }
+            var cep = "70150900";
+            
+            var result = new CEPAbertoV3().Make(cep);
 
-        public class ByNearest : CEPAbertoRequest
-        {
-            public ByNearest(String token, Tuple<String,String>[] parameters) : base(token, parameters) { }
-            public override String Resource
-            {
-                get { return "nearest"; }
-            }
-            public override String[] AvaiableParameters
-            {
-                get { return new String[] { "lat", "lng" }; }
-            }
+            Console.WriteLine("Logradouro --> {0}", result.logradouro); Console.ReadKey();
         }
     }
 }
