9a3451ce7dce67739b443c35331e70804d66e908
eduardom <edumucelli@gmail.com>
2020-04-11 18:45:17 +0200

    Remove V2 since the API has been deprecated

--- api/v2/api.php
+++
@@ -1,7 +0,0 @@
-$token = 'YOUR_TOKEN';
-$url = 'http://www.cepaberto.com/api/v2/ceps.json?cep=' . $cep;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Token token="' . $token . '"'));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$output = curl_exec($ch);
--- api/v2/api.sh
+++
@@ -1,1 +0,0 @@
-curl http://www.cepaberto.com/api/v2/ceps.xml?cep=40010000 -H 'Authorization: Token token="YOUR_TOKEN"'
